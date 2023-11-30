70778fae68182c6ee5138d8c8fbf5499534b0bb0
MarcoDC96 <marco.decicco@live.it>
2023-11-30 14:32:13 +0100

    Finish without bonus

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,28 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] invitatiFerragnez = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", " Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        System.out.print("Dimmi il tuo nome e cognome: ");
+        String tuoNomeCognome = scan.nextLine();
+
+        boolean nomeNellaLista = false;
+        for (int i = 0; i < invitatiFerragnez.length && !nomeNellaLista ; i++) {
+            if (tuoNomeCognome.equals(invitatiFerragnez[i])) {
+                nomeNellaLista = true;
+            }
+        }
+        if(!nomeNellaLista){
+            System.out.println("Non sei in lista, Vattene.");
+        }
+        else{
+            System.out.println("Sei in lista, prego entri!");
+        }
+
+        scan.close();
+    }
 }
